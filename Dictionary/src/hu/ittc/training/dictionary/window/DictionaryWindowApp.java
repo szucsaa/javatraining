e7a94259af6571e4ef1c9dd5b60fa1e3c17fe07a
Miklós Sipkó <miklos.sipko@telekom.com>
2021-07-01 21:02:37 +0200

    "Not working properly"

--- Dictionary/src/hu/ittc/training/dictionary/window/DictionaryWindowApp.java
+++ Dictionary/src/hu/ittc/training/dictionary/window/DictionaryWindowApp.java
@@ -1,9 +1,12 @@
 package hu.ittc.training.dictionary.window;
 
+import hu.ittc.training.dictionary.model.Book;
 import hu.ittc.training.dictionary.model.BookShelf;
 import hu.ittc.training.dictionary.window.eventlistener.OpenBookMouseEventListener;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DictionaryWindowApp {
 
@@ -19,6 +22,25 @@ public class DictionaryWindowApp {
         file.add(openBook);
         file.add(openDictionary);
         JMenu translate =  new JMenu("Translate");
+
+
+        ArrayList<Book> bookshelf = new ArrayList<>();
+        Book book1 = new Book();
+        book1.setName("The Great Gatsby");
+        Book book2 = new Book();
+        book2.setName("The Godfather");
+        Book book3 = new Book();
+        book3.setName("Crime and Punishment");
+        Book book4 = new Book();
+        book4.setName("Moby-Dick");
+        bookshelf.add(book1);
+        bookshelf.add(book2);
+        bookshelf.add(book3);
+        bookshelf.add(book4);
+
+        bookshelf.forEach(bk -> translate.add(new JMenuItem(bk.getName())));
+
+
         jMenuBar.add(file);
         jMenuBar.add(translate);
 
